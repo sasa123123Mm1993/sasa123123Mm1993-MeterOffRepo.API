c236b41f020fd910d1cb71b44599520b2ce48f60
MohammedHosnii <115946208+MohammedHosnii@users.noreply.github.com>
2024-08-20 13:31:35 +0300

    update program file

--- MeterOff.API/Program.cs
+++ MeterOff.API/Program.cs
@@ -83,11 +83,13 @@ builder.Logging.AddConsole();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
